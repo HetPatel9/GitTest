5cc6a616e7da16a276886e0009fe70751f5af930
HetPatel9 <het.p@simformsolutions.com>
2023-02-13 12:01:11 +0530

    commit diff

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ public class Diff{
     public static void main(String [] args){
     int a = 4;
     int b = 3;
-    System.out.println("Sum is " +(a+b));
+    System.out.println(" difference" is " +(a+b));
     }
 }
