d98547d6c64fc667b848936a07e057c5e60da981
HetPatel9 <het.p@simformsolutions.com>
2023-02-17 10:24:43 +0530

    edit text in mult.java output

--- mult.java
+++ mult.java
@@ -2,6 +2,6 @@ public class Diff{
     public static void main(String [] args){
     int a = 4;
     int b = 3;
-    System.out.println("Multiply is " +(a*b));
+    System.out.println("Multiply of 2 number is " +(a*b));
     }
 }
